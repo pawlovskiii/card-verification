3804d76c7393db267e2cdcd3ba8d8a9ebbb5420d
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-06-30 19:06:01 +0200

    Finished the first validation section with the tests for every single case

--- docs/scripts/checkCardNumber.js
+++ docs/scripts/checkCardNumber.js
@@ -1,5 +1,10 @@
 function checkCardNumber(cardNumber) {
+	if (typeof cardNumber !== 'number') throw Error('Input must be a number.');
+	if (!/^(\d{13}|\d{15,16})$/.test(cardNumber)) throw Error('Input must contains 13, 15 or 16 digits.');
+	if (!['50', '51', '52', '53', '54', '55', '4', '34', '37'].includes(String(cardNumber).match(/^\d\d/g).join(''))) throw Error('First two digits must be from range [[51-55], 34, 37, 4]');
 	return cardNumber;
 }
+
+console.log(checkCardNumber(553456789012313));
 
 export { checkCardNumber };
--- docs/scripts/checkCardNumber.test.js
+++ docs/scripts/checkCardNumber.test.js
@@ -1,1 +1,31 @@
-import { checkCardNumber } from "./checkCardNumber.js";
+import { checkCardNumber } from './checkCardNumber.js';
+
+test('should throw an error if the input is the wrong type', () => {
+	expect(() => checkCardNumber('abcde')).toThrow('Input must be a number.');
+	expect(() => checkCardNumber(null)).toThrow('Input must be a number.');
+	expect(() => checkCardNumber([1, 2, 3]).toThrow('Input must be a number.'));
+});
+
+test('should throw an error if the input contains the wrong number of digits', () => {
+	expect(() => checkCardNumber(102030405060)).toThrow(
+		'Input must contains 13, 15 or 16 digits.'
+	);
+	expect(() => checkCardNumber(12345678912345678)).toThrow(
+		'Input must contains 13, 15 or 16 digits.'
+	);
+	expect(() => checkCardNumber(12345678912345)).toThrow(
+		'Input must contains 13, 15 or 16 digits.'
+	);
+});
+
+test('should throw an error if the first two digits are not from certain range', () => {
+	expect(() => checkCardNumber(563456789012313)).toThrow(
+		'First two digits must be from range [[51-55], 34, 37, 4]'
+	);
+	expect(() => checkCardNumber(5995840321129)).toThrow(
+		'First two digits must be from range [[51-55], 34, 37, 4]'
+	);
+	expect(() => checkCardNumber(3503948576312345)).toThrow(
+		'First two digits must be from range [[51-55], 34, 37, 4]'
+	);
+});
