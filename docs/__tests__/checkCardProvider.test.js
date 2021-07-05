74d2690c41521a391d796cea32a4955a786e2e56
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-05 16:46:56 +0200

    Improved the structure of the tests with the description

--- docs/__tests__/checkCardProvider.test.js
+++ docs/__tests__/checkCardProvider.test.js
@@ -1,12 +1,18 @@
 import { checkCardProvider } from '../scripts/checkCardProvider.js';
 
-test(`should return the name of the card's provider`, () => {
-	expect(checkCardProvider(378282246310005)).toBe('American Express');
-	expect(checkCardProvider(378734493671000)).toBe('American Express');
-	expect(checkCardProvider(2221000000000009)).toBe('Mastercard');
-	expect(checkCardProvider(5555555555554444)).toBe('Mastercard');
-	expect(checkCardProvider(5105105105105100)).toBe('Mastercard');
-	expect(checkCardProvider(4111111111111111)).toBe('Visa');
-	expect(checkCardProvider(4012888888881881)).toBe('Visa');
-	expect(checkCardProvider(4222222222222)).toBe('Visa');
+describe(`should return the name of the card's provider`, () => {
+	test('American Express', () => {
+		expect(checkCardProvider(378282246310005)).toBe('American Express');
+		expect(checkCardProvider(378734493671000)).toBe('American Express');
+	});
+	test('Mastercard', () => {
+		expect(checkCardProvider(2221000000000009)).toBe('Mastercard');
+		expect(checkCardProvider(5555555555554444)).toBe('Mastercard');
+		expect(checkCardProvider(5105105105105100)).toBe('Mastercard');
+	});
+	test('Visa', () => {
+		expect(checkCardProvider(4111111111111111)).toBe('Visa');
+		expect(checkCardProvider(4012888888881881)).toBe('Visa');
+		expect(checkCardProvider(4222222222222)).toBe('Visa');
+	});
 });
--- docs/__tests__/isCardNumberValid.test.js
+++ docs/__tests__/isCardNumberValid.test.js
@@ -1,19 +1,23 @@
 import { isCardNumberValid } from '../scripts/cardNumberVerifier';
 import { cardValidation } from '../scripts/cardValidation.js';
 
-test('should throw an error if the input is the wrong type', () => {
-	expect(() => cardValidation('abcde')).toThrow('Input must be a number.');
-	expect(() => cardValidation(null)).toThrow('Input must be a number.');
-	expect(() => cardValidation([1, 2, 3]).toThrow('Input must be a number.'));
+describe('Initial input validation', () => {
+	test('should throw an error if the input is the wrong type', () => {
+		expect(() => cardValidation('abcde')).toThrow('Input must be a number.');
+		expect(() => cardValidation(null)).toThrow('Input must be a number.');
+		expect(() => cardValidation([1, 2, 3]).toThrow('Input must be a number.'));
+	});
 });
 
-test('should return true if the card verification is correct', () => {
-	expect(isCardNumberValid(5193080150954111)).toBe(true);
-	expect(isCardNumberValid(378282246310005)).toBe(true);
-	expect(isCardNumberValid(378734493671000)).toBe(true);
-	expect(isCardNumberValid(5105105105105100)).toBe(true);
-	expect(isCardNumberValid(2223000048400011)).toBe(true);
-	expect(isCardNumberValid(4012888888881881)).toBe(true);
-	expect(isCardNumberValid(4222222222222)).toBe(true);
-	expect(isCardNumberValid(4111111111111111)).toBe(true);
+describe(`Checking the correctness with the Luhn's algorithm`, () => {
+	test('should return true if the card number is correct', () => {
+		expect(isCardNumberValid(5193080150954111)).toBe(true);
+		expect(isCardNumberValid(378282246310005)).toBe(true);
+		expect(isCardNumberValid(378734493671000)).toBe(true);
+		expect(isCardNumberValid(5105105105105100)).toBe(true);
+		expect(isCardNumberValid(2223000048400011)).toBe(true);
+		expect(isCardNumberValid(4012888888881881)).toBe(true);
+		expect(isCardNumberValid(4222222222222)).toBe(true);
+		expect(isCardNumberValid(4111111111111111)).toBe(true);
+	});
 });
